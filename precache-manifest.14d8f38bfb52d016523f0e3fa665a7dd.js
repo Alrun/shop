bad992a9746e80b5475ea41e8777c9137610a16d
Alrun-PC <fkhey2016@yandex.ru>
2019-10-14 06:08:31 +0300

    Updates

---
+++ precache-manifest.14d8f38bfb52d016523f0e3fa665a7dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e455f4351745586b5f28c8f9f69dcbb",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "b0ea94d8f502b6e09df5",
+    "url": "/shop/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8a076caf92b46c38b814",
+    "url": "/shop/static/css/main.e7638e52.chunk.css"
+  },
+  {
+    "revision": "b0ea94d8f502b6e09df5",
+    "url": "/shop/static/js/2.4a4571f7.chunk.js"
+  },
+  {
+    "revision": "8a076caf92b46c38b814",
+    "url": "/shop/static/js/main.c84f1cbd.chunk.js"
+  },
+  {
+    "revision": "570511d975a97751173e",
+    "url": "/shop/static/js/runtime-main.cc83bde6.js"
+  },
+  {
+    "revision": "dc7d4c832be5b2cccc2b5e170f48f64c",
+    "url": "/shop/static/media/ProximaNova_Light.dc7d4c83.ttf"
+  },
+  {
+    "revision": "183dbd84910e682c850f0d63f759003c",
+    "url": "/shop/static/media/ProximaNova_Regular.183dbd84.ttf"
+  },
+  {
+    "revision": "0db0c57ce1bc39c8c740a100219deb8e",
+    "url": "/shop/static/media/ProximaNova_Semibold.0db0c57c.ttf"
+  }
+]);
